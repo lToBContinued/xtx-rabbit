18b55d2b6dba63a6555588ec55c0b6d945272195
lToBContinued <3212834434@qq.com>
2024-04-27 17:07:27 +0800

    完成主题色定制

--- vite.config.js
+++ vite.config.js
@@ -16,13 +16,24 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        // 1. 配置element-plus采用scss样式配色系统
+        ElementPlusResolver({ importStyle: 'sass' })
+      ]
     })
   ],
   resolve: {
     base: './',
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
     }
   }
 })
