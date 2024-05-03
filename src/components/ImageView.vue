ded2b54037bb8414f34bc0eb9850c0e06d2d6280
lToBContinued <3212834434@qq.com>
2024-05-03 12:24:28 +0800

    图片预览组件-放大镜功能以及优化

--- src/components/ImageView.vue
+++ src/components/ImageView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 
 // 图片列表
 const imageList = [
@@ -15,6 +16,47 @@ const activeIndex = ref(0)
 const enterHandeler = (i) => {
   activeIndex.value = i
 }
+
+// 2、蒙层跟随鼠标移动
+const target = ref(null)
+// 获取鼠标相对位置
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+// 监听elementX和elementY的变化，一旦变化，重置蒙层位置（left和top）
+const left = ref(0)
+const top = ref(0)
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX, elementY, isOutside], () => {
+  // 如果鼠标没有移动到盒子中，不执行放大等逻辑
+  if (!isOutside.value) {
+    // 有效范围内控制滑块距离
+    // 横向
+    if (elementX.value > 100 && elementX.value < 300) {
+      left.value = elementX.value - 100
+    }
+    // 纵向
+    if (elementY.value > 100 && elementY.value < 300) {
+      top.value = elementY.value - 100
+    }
+    // 边界距离控制
+    if (elementX.value > 300) {
+      left.value = 200
+    }
+    if (elementX.value < 100) {
+      left.value = 0
+    }
+    if (elementY.value > 300) {
+      top.value = 200
+    }
+    if (elementY.value < 100) {
+      top.value = 0
+    }
+
+    // 控制大图显示
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
+  }
+})
 </script>
 
 <template>
@@ -23,7 +65,11 @@ const enterHandeler = (i) => {
     <div ref="target" class="middle">
       <img :src="imageList[activeIndex]" alt="" />
       <!--蒙层小块-->
-      <div :style="{ left: '0px', top: '0px' }" class="layer"></div>
+      <div
+        v-show="!isOutside"
+        :style="{ left: `${left}px`, top: `${top}px` }"
+        class="layer"
+      ></div>
     </div>
 
     <!--小图列表-->
@@ -40,12 +86,12 @@ const enterHandeler = (i) => {
 
     <!--放大镜图-->
     <div
-      v-show="false"
+      v-show="!isOutside"
       :style="[
         {
-          backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`
+          backgroundImage: `url(${imageList[activeIndex]})`,
+          backgroundPositionX: `${positionX}px`,
+          backgroundPositionY: `${positionY}px`
         }
       ]"
       class="large"
